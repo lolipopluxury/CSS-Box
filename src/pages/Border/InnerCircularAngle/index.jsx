1a299c0450c19d25a7778efdccd454e96ba1991b
lolipopluxury <songyupeng@yahoo.com>
2021-04-30 21:13:09 +0800

    monaco-editor set up

--- src/pages/Border/InnerCircularAngle/index.jsx
+++ src/pages/Border/InnerCircularAngle/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Row, Col } from 'antd';
+import { Card, Row, Col, Typography } from 'antd';
 import { FormattedMessage } from 'umi';
 import styles from './index.less'
+import { BackgroundColor } from 'chalk';
+import Editor from '@monaco-editor/react'
+
+const cssText = 
+`.target {
+  width: 30em;
+  height: 8em;
+  margin: auto;
+  background: tan;
+  border-radius: .8em;
+  padding: 1em;
+  box-shadow: 0 0 0 .4em rgb(196, 124, 124);
+  outline: .6em solid rgb(196, 124, 124);
+}`
 
 export default () => {
   return (
@@ -11,7 +25,12 @@ export default () => {
         <h2><FormattedMessage id="context.border.inner-circular-border.title"/></h2>
         <Row>        
           <Col span={12}>          
-            <p>sakdaskldaskldaskldaskljdksaljdkaldjaksl</p>
+            <Editor
+              height="30vh"
+              theme="vs-dark"
+              defaultLanguage="css"
+              defaultValue={cssText}          
+            />
           </Col>
           <Col span={12}>
             <div className={styles.target}>        
