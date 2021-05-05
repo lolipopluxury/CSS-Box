1380c96298d628d9c16fcfd06a91681a3c38008d
lolipopluxury <songyupeng@yahoo.com>
2021-05-05 08:37:24 +0800

    highlighter

--- src/pages/Border/InnerCircularAngle/index.jsx
+++ src/pages/Border/InnerCircularAngle/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Row, Col, Typography, Divider } from 'antd';
+import { Card, Row, Col, Divider } from 'antd';
 import { FormattedMessage } from 'umi';
-import styles from './index.less'
-import Editor from '@monaco-editor/react'
+import styles from './index.less';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { stackoverflowLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-const cssText = 
-`.target {
+const cssText = `.target {
   width: 30em;
   height: 8em;
   margin: auto;
@@ -15,31 +15,32 @@ const cssText =
   padding: 1em;
   box-shadow: 0 0 0 .4em rgb(196, 124, 124);
   outline: .6em solid rgb(196, 124, 124);
-}`
-
-const { Text } = Typography
+}`;
 
 export default () => {
   return (
     <PageContainer>
       <Card>
-        <h2><FormattedMessage id="context.border.inner-circular-border.title"/></h2>
-        <Divider orientation="left" className={styles.keywords}>border-radius & box-shadow & outline</Divider>      
-        <Row>        
-          <Col span={12}>          
-            <Editor
-              height="30vh"
-              theme="light"
-              defaultLanguage="css"
-              defaultValue={cssText}          
-            />
+        <h2>
+          <FormattedMessage id="context.border.inner-circular-border.title" />
+        </h2>
+        <Divider orientation="left" className={styles.keywords}>
+          border-radius & box-shadow & outline
+        </Divider>
+        <Row>
+          <Col span={16}>
+            <SyntaxHighlighter
+              language="css"
+              style={stackoverflowLight}
+              className={styles.highlighter}
+            >
+              {cssText}
+            </SyntaxHighlighter>
           </Col>
-          <Col span={12}>
-            <div className={styles.target}>        
-              I have a nice inner
-            </div>
+          <Col span={8}>
+            <div className={styles.target}>I have a nice inner</div>
           </Col>
-        </Row>      
+        </Row>
       </Card>
     </PageContainer>
   );
