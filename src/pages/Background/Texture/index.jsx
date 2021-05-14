2cb849f6c33a45f836d1cbddeedb6f67cc68ff18
lolipopluxury <songyupeng@yahoo.com>
2021-05-14 22:23:47 +0800

    update

--- src/pages/Background/Texture/index.jsx
+++ src/pages/Background/Texture/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import { FormattedMessage } from 'umi';
-import { Card } from 'antd';
+import { Card, Row, Col, Divider, Typography, Space } from 'antd';
+import styles from './index.less';
+
+const { Paragraph } = Typography;
 
 export default () => {
   return (
@@ -10,6 +13,21 @@ export default () => {
         <h2>
           <FormattedMessage id="context.background.texture.title" />
         </h2>
+        <Divider orientation="left" className={styles.keywords}>
+          Keywords: linear-gradient & background-size
+        </Divider>
+        <Space direction="vertical" size="small">
+          <Row>
+            <Col span={1} />
+            <Col span={18}>
+              <Paragraph>
+                <blockquote>
+                  <FormattedMessage id="context.border.inner-circular-border.paragraph.first" />
+                </blockquote>
+              </Paragraph>
+            </Col>
+          </Row>
+        </Space>
       </Card>
     </PageContainer>
   );
